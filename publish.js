00954f4bf6413727d9ea2494c4d1a2ca1fc07fa4
baochun <baochun.xie@ericsson.com>
2019-06-20 12:54:07 +0800

    dapuye

--- publish.js
+++ publish.js
@@ -1,10 +1,12 @@
 require(`shelljs/global`);
 cd('dest');
+exec('git checkout master');
 exec('git add .');
 if(exec('git commit -m "update"').code !==0){
 	echo(`Error: Git commit master failed`);
     exit(1);
 }
+exec('git pull origin master');
 if (exec(`git push origin master`).code !== 0) {
 	echo(`Error: Git push master failed`);
 	exit(1);
